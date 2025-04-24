51aaa8caf9e9a9b5b50b513dedff9cae74143d28
Ashish <ashish74624@gmail.com>
2025-04-24 18:57:49 +0530

    Route ts file

--- server/index.ts
+++ server/index.ts
@@ -1,26 +1,56 @@
-import express from "express";
-import { fileURLToPath } from "url";
+import express, { Request, Response, NextFunction } from "express";
+import expressHandlebars from "express-handlebars";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 import path from "path";
 
-/* __dirname/filename equivalents in ESM */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import config from "../server/config.json"
+import router from "./router"
+import init from "./init";
+import handlebars from "./handlebars";
 
 const app = express();
-const PORT = process.env.PORT ?? 3001;
+const port = 3000;
 
-/* simple JSON body parser */
-app.use(express.json());
+handlebars();
 
-app.get("/", (_req, res) => {
-    res.send("Hello from Express + TypeScript (ESM)!");
+// Connect to Database
+const databaseUri: string = config.database.uri;
+const databaseOptions = config.database.options;
+
+mongoose.connect(databaseUri, databaseOptions as mongoose.ConnectOptions, async (error) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    console.log("MongoDB connected");
+    init();
 });
 
-/* example POST route */
-app.post("/echo", (req, res) => {
-    res.json({ youSent: req.body });
+// Set Template Engine
+app.engine("handlebars", expressHandlebars({
+    layoutsDir: path.join(__dirname, "../views/layouts/"),
+    partialsDir: path.join(__dirname, "../views")
+}));
+app.set("view engine", "handlebars");
+
+// Set Middlewares
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.static("public"));
+app.use(router);
+
+// Error Handling 404, 500
+app.use((req: Request, res: Response) => {
+    console.warn("404 Page Not Found", req.url);
+    res.sendStatus(404);
 });
 
-app.listen(PORT, () =>
-    console.log(`🚀  Server running at http://localhost:${PORT}`)
-);
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    res.sendStatus(500);
+});
+
+app.listen(port, () => {
+    console.log("Server is running on port", port);
+});
